cec08388e322a6631cb43e050456d2a93848311b
Wyatt Kirby <kirby.wa@gmail.com>
2014-02-28 19:17:41 -0800

    Improve Footer Formatting

--- footer.js
+++ footer.js
@@ -1,7 +1,7 @@
 exports.header = null
 
 exports.footer = {
-	height: "1cm",
+	height: '.5in',
 	contents: function(pageNum, numPages) {
 		var today = new Date(),
 			dd = today.getDate(),
@@ -15,27 +15,17 @@ exports.footer = {
 		if ( mm < 10 ) { mm = '0' + mm; }
 		today = mm + '/' + dd + '/' + yyyy;
 
-		returnHTML += '<table border="0" cellspacing="0" cellpadding="0" width="100%" style="font-size: 8pt; font-family: open sans, sans-serif; color: #999; border-collapse: collapse; margin: 0; padding: 0; vertical-align: bottom;">';
-			returnHTML += '<tbody>';
-				returnHTML += '<tr>';
-					returnHTML += '<td style="padding: 0; vertical-align: bottom;">' + today +'</td>';
-					returnHTML += '<td style="padding: 0; vertical-align: bottom; text-align: right;">' + pageNum + " of " + numPages +'</td>';
-				returnHTML += '</tr>';
-			returnHTML += '</tbody>';
-		returnHTML += '</table>';
+		returnHTML += '<div style="padding-top: .25in;">';
+			returnHTML += '<table border="0" cellspacing="0" cellpadding="0" width="100%" style="font-size: 8pt; font-family: open sans, sans-serif; color: #999; border-collapse: collapse; margin: 0; padding: 0;">';
+				returnHTML += '<tbody>';
+					returnHTML += '<tr>';
+						returnHTML += '<td style="padding: 0; vertical-align: bottom;">' + today +'</td>';
+						returnHTML += '<td style="padding: 0; vertical-align: bottom; text-align: right;">' + pageNum + " of " + numPages +'</td>';
+					returnHTML += '</tr>';
+				returnHTML += '</tbody>';
+			returnHTML += '</table>';
+		returnHTML += '</div>';
 
 		return returnHTML;
 	}
 }
-
-/**
- * header and footer might be of format specified in https://github.com/ariya/phantomjs/wiki/API-Reference-WebPage#wiki-webpage-paperSize
- * 
- * Example:
- *  {
- *    height: "1cm",
- *    contents: function(pageNum, numPages) {
- *      return "<h1>Header <span style='float:right'>" + pageNum + " / " + numPages + "</span></h1>"
- *    }
- *  }
- */
